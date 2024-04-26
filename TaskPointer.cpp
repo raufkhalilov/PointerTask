1df4031d08a39c5778f9839f8498ff184882bd5a
raufkhalilov <rauf.khalilov.2004@mail.ru>
2024-04-26 19:15:07 +0300

    Main code

---
+++ TaskPointer.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <memory>
+
+int main() {
+    //с использованием "сырых" указателей
+    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int* ptr1 = &arr[0];
+    std::cout << "4th element of the array: " << *(ptr1 + 3) << std::endl;
+
+    //с использованием умных указателей
+    std::unique_ptr<int[]> arr2(new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    int* ptr2 = arr2.get();
+    std::cout << "4th element of the array: " << *(ptr2 + 3) << std::endl;
+
+    return 0;
+}
